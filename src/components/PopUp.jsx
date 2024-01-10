d64cd51d3d404a7f8bad935600606726e7f8ec6d
SherazKarim <sherazkarimz@gmail.com>
2024-01-10 23:16:31 +0500

    pushed

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './PopUp.module.css';
+import maper from "../images/maper.png";
+const PopUp = ({ winPrize, onClaimBonus , handleCloseBtn}) => {
+  return (
+    <div className={styles.container2}>
+      <div className={styles.prize}>
+       {winPrize === "Empty" ?  <h1>Better Luck Next Time!</h1> :  <h1>Congratulations you <br /> <span className={styles.span1}>Won</span>
+          <span>${winPrize} + 200</span></h1> }
+      </div>
+      <div className={styles.footer}>
+        <img className={styles.btn_image} src={maper} alt="" />
+       {winPrize === "Empty" ? ""  :<a  href='https://basari.bet/register/?atp=&goto=sitereg&click_id=db0e6b614084ed125e69c0acdbe0155a&plid=18023&bnid=28892&lang=en&cc=DE&pid=75001&timestamp=1704818372.0174&refCode=mb_BQBnRgAA3HAAAPkkAQA.2024-01.10&uuid=2241693eef5ab9219d7c3219c3c69d9c789906dc' className={styles.PopUpButton} onClick={onClaimBonus}>
+          Claim Bonus
+        </a>}
+      </div>
+      <div className={styles.close}>
+        <button onClick={handleCloseBtn}>close</button>
+      </div>
+    </div>
+  );
+}
+
+export default PopUp;
--- src/index.js
+++ src/index.js
@@ -15,3 +15,149 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// import React, { useState, useRef, useEffect } from 'react';
+// import './App.css';
+// import bird1 from "./images/paret.webp";
+// import bird2 from "./images/paret2.webp";
+// import star1 from "./images/star1.webp";
+// import star2 from "./images/star2.webp";
+// import arrow from "./images/arrow.webp";
+// import spin from "./images/btn.png";
+// import PopUp from './components/PopUp';
+// import bg from "./images/bg3.webp";
+// import startSound from './audio/start.mp3';
+// import winSound from './audio/win.mp3';
+// import loseSound  from "./audio/lose.mp3"
+// function App() {
+//   const [clicks, setClicks] = useState(0);
+//   const [rotation, setRotation] = useState(0);
+//   const [showPopUp, setShowPopUp] = useState(false);
+//   const [prices, setPrices] = useState({
+//     price1: "Empty",
+//     price2: 400,
+//     price3: 1000,
+//     price4: 15,
+//     price5: "Empty",
+//     price6: 300,
+//     price7: 5000,
+//     price8: 600,
+//   });
+//   const [winPrize, setWinPrize] = useState();
+
+//   const startAudioRef = useRef(null);
+//   const winAudioRef = useRef(null);
+// const loseAudioRef = useRef(null)
+//   useEffect(() => {
+//     const popUpShown = localStorage.getItem('popUpShown') === 'true';
+//     const savedClicks = parseInt(localStorage.getItem('clicks'), 10) || 0;
+//     const savedWinPrize = localStorage.getItem('winPrize');
+//     setShowPopUp(popUpShown);
+//     setClicks(savedClicks);
+//     if (savedWinPrize) {
+//       setWinPrize(savedWinPrize);
+//     }
+//   }, []);
+
+//   const handleSpinClick = () => {
+//     if (clicks > 0) {
+//       return;
+//     }
+//     setClicks(1);
+//     localStorage.setItem('clicks', '1');
+
+//     const newRotation = rotation + 360 + Math.floor(Math.random() * 1000);
+//     setRotation(newRotation);
+//     startAudioRef.current.play();
+
+//     setTimeout(() => {
+//       const finalRotation = newRotation % 360;
+//       const offsetAngle = (385 - finalRotation + 22.5) % 360;
+//       const winningSector = Math.floor(offsetAngle / 40);
+//       let winningPrice;
+
+//       if (winningSector > 7) {
+//         winningPrice = prices.price8;
+//       } else if (winningSector === 1) {
+//         winningPrice = prices.price1;
+//       } else if (winningSector === 0) {
+//         winningPrice = prices.price2;
+//       } else {
+//         winningPrice = prices[`price${winningSector + 1}`];
+//       }
+
+//       localStorage.setItem('winPrize', winningPrice);
+//       setWinPrize(winningPrice);
+//      {winningPrice === "Empty" ? loseAudioRef.current.play(): winAudioRef.current.play();}
+
+
+//       localStorage.setItem('popUpShown', 'true');
+//       setShowPopUp(true);
+//     }, 5000);
+//   };
+
+//   const handleClaimBonus = () => {
+//     setShowPopUp(true);
+//     setClicks(1);
+//     localStorage.setItem('popUpShown', 'true');
+//     localStorage.setItem('clicks', '1');
+//     // localStorage.removeItem('winPrize');
+//     // setWinPrize(undefined);
+//   };
+
+//   const handleCloseBtn = () =>{
+//     setShowPopUp(false);
+//     setClicks(0);
+//     localStorage.setItem('popUpShown', 'false');
+//     localStorage.setItem('clicks', '0');
+//     localStorage.removeItem('winPrize');
+//     setWinPrize(undefined);
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <div id="spin" onClick={handleSpinClick}>
+//         <img src={spin} alt="Spin" />
+//       </div>
+//       <div className='birds'>
+//         <img className='bird-1' src={bird1} alt="Bird 1" />
+//         <img className='bird-2' src={bird2} alt="Bird 2" />
+//       </div>
+//       <div className='stars'>
+//         <img className='bird-1' src={star1} alt="Bird 1" />
+//         <img className='bird-2' src={star2} alt="Bird 2" />
+//       </div>
+//       <div className='design'>
+//         <img src={bg} width={500} height={500} alt="Background" />
+//       </div>
+//       <span className="arrow">
+//         <img src={arrow} alt="Arrow" />
+//       </span>
+//       <div className="container" style={{ transform: `rotate(${rotation}deg)` }}>
+//         <div className="one">{prices.price1}</div>
+//         <div className="eight">{prices.price2} Fs</div>
+//         <div className="two">${prices.price3}</div>
+//         <div className="three">{prices.price4} Fs</div>
+//         <div className="four">{prices.price5}</div>
+//         <div className="five">${prices.price6}</div>
+//         <div className="six">{prices.price7} Fs</div>
+//         <div className="seven">${prices.price8}</div>
+//       </div>
+//       {showPopUp && clicks > 0 && (
+//         <PopUp winPrize={winPrize} onClaimBonus={handleClaimBonus} handleCloseBtn={handleCloseBtn}/>
+//       )}
+
+//       <audio ref={startAudioRef} src={startSound} preload="auto"></audio>
+//       <audio ref={winAudioRef} src={winSound} preload="auto"></audio>
+//       <audio ref={loseAudioRef} src={loseSound} preload="auto"></audio>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
